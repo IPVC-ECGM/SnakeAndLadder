5f13080a8f5d1c5f0b6d710e18beb1297e93be22
kanzaky <kanzakycloud@gmail.com>
2022-05-02 00:31:11 +0100

    Variáveis Gerais

--- src/main/java/ecgm/snakeandladder/snakeandladder/Game.java
+++ src/main/java/ecgm/snakeandladder/snakeandladder/Game.java
@@ -27,6 +27,36 @@ public class Game extends Application {
     public static final int height = 10;
     public static final int but_area = 80;
 
+    //Variéveis para os dados
+    public int randNum;
+    public Label randomResult;
+
+    //Pião/Jogador
+    public Circle player1;
+    public Circle player2;
+    //
+    public int playerPos[][] = new int [10] [10];
+    //public int ladderPos[][] = new int [6] [3];
+    //Posição do Jogador
+    public int player_1_position = 1;
+    public int player_2_position = 1;
+    //Responsável por saber qual é a vez de cada jugador jugar
+    public boolean player_1_Turn = true;
+    public boolean player_2_Turn = true;
+    //PosiçãoX do jpgador - de forma que fique no centro do quadradinho
+    public static int player_1_XPos = 40;
+    public static int player_1_YPos = 760;
+    public static int player_2_XPos = 40;
+    public static int player_2_YPos = 760;
+
+    //Responsavel pelo inicio do jogo / caso false ninguem pode rodar o dado
+    public boolean gameStart = false;
+    public Button gameButton;
+
+    //
+    public int posPlay1 = 1;
+    public int posPlay2 = 1;
+
     //Variável responsevel por guardar cada Quadradinho nesse grupo
     private Group SquareGroup = new Group();
     }
