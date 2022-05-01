5a552766780e35959989f7efc63b974add5d66b2
kanzaky <kanzakycloud@gmail.com>
2022-05-02 00:13:32 +0100

    Some Files Creation

--- src/main/java/ecgm/snakeandladder/snakeandladder/Controller.java
+++ src/main/java/ecgm/snakeandladder/snakeandladder/Controller.java
@@ -1,14 +1,30 @@
 package ecgm.snakeandladder.snakeandladder;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void swichToMenu(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("src/main/resources/ecgm/snakeandladder/snakeandladder/Menu.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void swichToGame(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("src/main/resources/ecgm/snakeandladder/snakeandladder/Menu.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 }
