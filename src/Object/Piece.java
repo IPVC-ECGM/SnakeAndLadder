c913966af27d0e7dfe0d8b0d1bc59760364b9761
suila00 <suilasilva00@gmail.com>
2022-06-22 04:25:16 +0100

    Player objeto player Piece objeto piece Resources controlador de diretorio

---
+++ src/Object/Piece.java
@@ -0,0 +1,41 @@
+package Object;
+
+import javafx.scene.image.ImageView;
+
+import java.io.Serializable;
+
+public class Piece implements Serializable {
+    private int position;
+    private String color;
+    private ImageView pieceImage;
+
+    public Piece(int position, String color, ImageView pieceImage) {
+        this.position = position;
+        this.color = color;
+        this.pieceImage = pieceImage;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public ImageView getPieceImage() {
+        return pieceImage;
+    }
+
+    public void setPieceImage(ImageView pieceImage) {
+        this.pieceImage = pieceImage;
+    }
+}
